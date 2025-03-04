044ac6cf3f842e722982d728abb10baffe9b53db
Cole Jensen <cole.jensen@yale.edu>
2025-03-04 15:48:01 -0500

    for beta testing

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
-# UCA
-This is for beta testing the unmutated common ancestor project within Dowser. 
+# Inferring the Unmuated Common Ancestor (UCA) in Dowser
+
+:construction: This readme is still under construction and is meant for *beta testers*. :construction:
+
+This is a script that will infer the naive sequence of a B cell clone. This process is jointly inferred using phylogenetic based statistics and the likelihood of a given B cell receptor (BCR) junction. 
+
+## Install the dependant R packages
+If you have not installed Dowser yet, you will be installing the developer version to test this pipeline. Dowser requires some bioconductor packages and some CRAN packages. As such, it tends to be easier to install the bioconductor packages before installing Dowser.
+
+```r
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+if (!require("Biostrings", quietly = TRUE)) {
+    BiocManager::install("Biostrings")
+}
+if (!require("ggtree", quietly = TRUE)) {
+    BiocManager::install("ggtree")
+}
+if (!require("GenomicAlignments", quietly = TRUE)) {
+    BiocManager::install("GenomicAlignments")
+}
+if (!require("IRanges", quietly = TRUE)) {
+    BiocManager::install("IRanges")
+}
+```
+
+## Downloading and installing the developer version of Dowser
+The internal R functions to run this script are contained in the developer version of Dowser.
+
+```r
+# Install the devtools package if not already installed
+if (!requireNamespace("devtools", quietly = TRUE)) {
+  install.packages("devtools")
+}
+
+# Load the devtools package
+library(devtools)
+
+# Install the developer version of dowser from GitHub
+install_github("immcantation/dowser")
+```
+
+## Install the required python packages 
+
+The following Python packages are required to run the script:
+
+- numpy
+- pandas
+- Bio (from Biopython)
+- multiprocessing
+- random
+- olga
+
+To install the required packages, you can use the following commands:
+
+```bash
+pip install numpy pandas biopython olga
+```
+
+## Installing IgPhyML
+
+In this process, tree base statistics will be used to infer the UCA. As such this process *involves* constructing trees. Currently the only tree building method supported is IgPhyML. Please make sure that you have installed IgPhyML. [Here are instructions that outline how to install IgPhyML](https://igphyml.readthedocs.io/en/latest/install.html). If you have successfully installed IgPhyML you should find a executable file in the 'src' folder called 'igphyml'. 
+
+## Clone Olga's Github
+
+Part of the UCA inference involves find the likelihood of a given junction sequence. We use `OLGA` to infer this. Please clone their github (you will need this later). 
+
+```bash
+git clone https://github.com/statbiophys/OLGA.git
+```
+
+## Preparing your data
+
+To do this, process your BCR data up to the formatClones step of building B cell phylogenies. To get this point, please follow [this tutorial](https://immcantation.readthedocs.io/en/stable/getting_started/10x_tutorial.html#build-and-visualize-trees). Some variant of the following should be the last step before anything new.
+
+```r
+library(dowser)
+# format the data into clones
+data <- formatClones(data)
+```
+## Inferring the UCA
+
+You should now have a clone object with at least one clone. You can now infer the UCA. To do so use the `getTrees_and_UCA` function in R. This function is dependent on a few things. A brief explanation will be included in the example below. 
+
+```r
+# clones = the clones object from formatClones
+# dir = the directory you want to save things to -- this can be NULL
+# build = this is the tree construction method you want to use. Currently **ONLY** 'igphmyl' works.
+# exec = the file path that leads to the igphyml executable
+# model_folder = the file path the leads to the olga heavy chain model files. This will be included in the 
+#                downloaded OLGA repo
+# uca_script = the file path to the script found in this repo
+# max_iters = the number of iterations you want to run -- the process usually needs 2-3 to resolve
+# id = the run id for igphyml and output folder naming
+# optimize = optimize HLP rates (r), lengths (l), and/or topology (r)
+# motifs = motifs to consider (see IgPhyMl docs)
+# hotness = hotness parameters to estimate (see IgPhyMl docs)
+# omega = omega parameters to estimate (see IgPhyMl docs)
+# rm_temp = a logical that tells the function to delete the files created in inference process
+# quiet = amount of rubbish to print to console
+# nproc = number of cores to parallelize computations 
+clones_UCA <- getTrees_and_UCA(clones = clones, dir = NULL, 
+                               build = "igphyml", exec = ".../igphyml/src/igphyml",
+                               model_folder = ".../human_B_heavy",
+                               uca_script = ".../get_UCA.py",
+                               max_iters = 10, id = "sample", optimize = "lr", 
+                               motifs = "WRC_2:0", hotness = 0, omega = "e", 
+                               rm_temp = FALSE, quiet = 1, nproc = 1)
+```
+
+The output of this function is your clones object with a new column `UCA` that contains the inferred UCA. This can be found in the data frame containing sequence information for each clone. 
+
+```r
+row_num <- # some number
+View(clones$data[[row_num]]@data$UCA[1])
+```
---
+++ UCA/get_UCA.py
@@ -0,0 +1,297 @@
+import argparse
+import subprocess
+import sys
+
+def install_and_import(package):
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    finally:
+        globals()[package] = __import__(package)
+
+install_and_import('numpy')
+install_and_import('pandas')
+install_and_import('Bio.Seq')
+install_and_import('multiprocessing')
+install_and_import('random')
+install_and_import('olga')
+
+from Bio.Seq import Seq
+from multiprocessing import Pool
+import numpy as np
+import olga.load_model as load_model
+import olga.generation_probability as pgen
+import pandas as pd 
+import random
+
+# Define the argument parser
+parser = argparse.ArgumentParser(description='Process UCA arguments.')
+parser.add_argument('--clone_ids', required=True, help='Clone IDs to get UCA for')
+parser.add_argument('--directory', required=True, help='Directory where the clone data is stored')
+parser.add_argument('--max_iters', type=int, required=True, help='Max number of iterations to run')
+parser.add_argument('--nproc', type=int, required=True, help='Number of processors to use')
+parser.add_argument('--id', required=True, help='The name of the folder that is created to store the data')
+parser.add_argument('--model_folder', required=True, help='The folder where the OLGA model information is stored')
+parser.add_argument('--quiet', type=int, default=0, help='Whether to print out the progress/messages of the script')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Print out the args 
+if args.quiet > 0:
+    print("Arguments: ", args)
+
+# right now input params are 
+# 0: get_UCA.py -- script name
+# 1: clone_ids -- clone ids to get UCA for
+# 2: directory -- directory where the clone data is stored
+# 3: max iters -- max number of iterations to run
+# 4: nproc -- number of processors to use
+# 5: id -- the name of the folder thats created to store the data
+# 6: model_folder -- the folder where the olga model information is stored
+# 7: quiet -- whether to print out the progress/messages of the script
+
+def find_all_indices(larger_string, smaller_string):
+    indices = []
+    start = 0
+    while True:
+        start = larger_string.find(smaller_string, start)
+        if start == -1:
+            break
+        indices.append(start)
+        start += 1  # Move past the last found index
+    return indices
+
+# now use the mcmc_codon_order to change the codons in the germline and get the pgen and new tree likelihood
+def generate_codon_list(codons, starting_germline, starting_point, end_point, cdr3_only):
+    if cdr3_only:
+        # find the codon locations of the CDR3
+        codons_cdr3 = [(i, starting_germline[i:i+3]) for i in range(starting_point, end_point, 3)]
+        positions = [pos for pos, _ in codons_cdr3]
+        new_order = random.sample(positions, len(positions))
+    else:
+        new_order = random.sample(range(len(codons)), len(codons))
+    return new_order
+
+# get the new propsed sequence
+def propose_new_combination(current_germline, codon_number, igphyml_df):
+    # swap the codon with another codon possibility at that site
+    current_codon = current_germline[codon_number:codon_number+3]
+    # filter down the igphyml table to only the rows that make up the current codon but don't include the current codon
+    sub_df = igphyml_df[igphyml_df["site"] == codon_number/3]
+    sub_df = sub_df[sub_df["codon"] != current_codon]
+    # pick a new codon 
+    new_codon = sub_df.sample(n=1)
+    # update the germline with the new codon
+    new_germline = current_germline[:codon_number] + new_codon["codon"].values[0] + current_germline[codon_number+3:]
+    return new_germline 
+    
+# test the seuqence for pgen and tree likelihood
+def get_new_lhood(proposed_combination, starting_point, ending_point, pgen_model, igphyml_df):
+    cdr3 = proposed_combination[starting_point:ending_point]
+    # get the new pgen
+    new_pgen = pgen_model.compute_nt_CDR3_pgen(cdr3)
+    if new_pgen == 0:
+        new_pgen = new_pgen + 1e-323
+    # get ln of it
+    new_pgen = np.log(new_pgen)
+    # get the new tree likelihood
+    new_codons = [proposed_combination[i:i+3] for i in range(0, len(proposed_combination), 3)]
+    # for loop to get the new tree likelihood
+    parital_likelihoods = []
+    # change this to match the cdr_only param -- rn use the full thing but change the starting germ to be 
+    # imgt V and J and MRCA cdr3
+    for i in range(0, igphyml_df['site'].max() + 1):
+        matched_value = igphyml_df.loc[(igphyml_df['site'] == i) & (igphyml_df['codon'] == new_codons[i]), 'partial_likelihood'].values[0]
+        parital_likelihoods.append(matched_value)
+    new_tree_likelihood = sum(parital_likelihoods)
+    # now put it all together
+    new_lhood = new_pgen + new_tree_likelihood
+    lhood_vector = [new_pgen, new_tree_likelihood, new_lhood]
+    return lhood_vector
+
+def translate_to_amino_acid(codon):
+    return str(Seq(codon).translate())
+
+# this is the parallized version of the above function
+def process_option(option, filtered_df, codon, current_germline, starting_point, ending_point, igphyml_df_new):
+    testing_df = filtered_df.iloc[option]
+    testing_germline = current_germline 
+    testing_germline = [testing_germline[i:i+3] for i in range(0, len(testing_germline), 3)]
+    testing_germline[codon // 3] = testing_df['codon']
+    testing_germline = ''.join(testing_germline)
+
+    # Now test the pgen stuff
+    new_lhoods = get_new_lhood(testing_germline, starting_point, ending_point, pgen_model, igphyml_df_new)
+    return testing_germline, new_lhoods
+
+def process_option_wrapper(args):
+    return process_option(*args)
+
+def get_updated_germline(starting_germline, starting_cdr3, igphyml_df, pgen_model, junction_length, cdr3_only=True, max_iter=100, nproc = 1):
+    current_codons = [starting_germline[i:i+3] for i in range(0, len(starting_germline), 3)]
+    current_germline = starting_germline
+    # get the inital position of the CDR3
+    starting_point = find_all_indices(starting_germline, starting_cdr3)[0]
+    ending_point = starting_point + int(junction_length)
+    current_lhoods = get_new_lhood(current_germline, starting_point, ending_point, pgen_model, igphyml_df)
+
+    # remove the nonviable site options for the starting and ending sites
+    starting_site = starting_point/3
+    ending_site = ending_point/3 - 1
+
+    # filtered down starting and ending options
+    starting_options = igphyml_df[igphyml_df["site"] == starting_site]
+    starting_options = starting_options[starting_options['codon'].apply(translate_to_amino_acid) == 'C']
+    ending_options = igphyml_df[igphyml_df["site"] == ending_site]
+    ending_options = ending_options[ending_options['codon'].apply(translate_to_amino_acid).isin(['W', 'F'])]
+
+    # the rest of em
+    filtered_igphyml_df = igphyml_df[~igphyml_df['site'].isin([starting_site, ending_site])]
+
+    # concat them 
+    igphyml_df_new = pd.concat([filtered_igphyml_df, starting_options, ending_options])
+    igphyml_df_new = igphyml_df_new.sort_values(by='site', ascending=True)
+
+
+    still_improving = True
+
+    # set up intermittent variables to track performance
+    tested_combinations = []
+    tested_lhoods_pgen = []
+    tested_lhoods_tree = []
+    tested_lhoods_combo = []
+    tested_iterations = []
+    iteration = 0
+
+    while still_improving and iteration < max_iter:
+        still_improving = False
+        codon_list = generate_codon_list(current_codons, current_germline, starting_point, ending_point, cdr3_only)
+        iteration += 1
+
+        for codon in codon_list:           
+            filtered_df = igphyml_df_new[igphyml_df_new["site"] == codon/3]
+            
+            with Pool(nproc) as pool:
+                results = pool.map(process_option_wrapper, [(option, filtered_df, codon, current_germline, starting_point, ending_point, igphyml_df_new) for option in range(0, len(filtered_df))])
+    
+            results_array = np.array([result[1] for result in results])
+            best_index = np.argmax(results_array[:, 2])
+
+            # track the performance of the MCMC
+            tested_combinations.append(results[best_index][0])
+            tested_lhoods_pgen.append(results[best_index][1][0])
+            tested_lhoods_tree.append(results[best_index][1][1])
+            tested_lhoods_combo.append(results[best_index][1][2])
+            indices = [i for i, x in enumerate(codon_list) if x == codon]
+            tested_iterations.extend([f"{iteration}_{index}" for index in indices])
+
+            if results_array[best_index][2] > current_lhoods[2]:
+                current_lhoods = list(results_array[best_index])
+                current_germline = results[best_index][0]
+                still_improving = True
+
+    data = {
+    'germline': tested_combinations,
+    'pgen_lhood': tested_lhoods_pgen,
+    'tree_lhood': tested_lhoods_tree,
+    'combo_lhood': tested_lhoods_combo,
+    'iteration': tested_iterations,
+    }
+
+    data = pd.DataFrame(data)
+
+    return current_germline, current_lhoods, data
+
+# Define the files for loading in generative model/data
+# TODO: make these command line arguments? Or should we just include it with the package?
+params_file_name = args.model_folder + '/model_params.txt'
+marginals_file_name = args.model_folder + '/model_marginals.txt'
+V_anchor_pos_file = args.model_folder + '/V_gene_CDR3_anchors.csv'
+J_anchor_pos_file = args.model_folder + '/J_gene_CDR3_anchors.csv'
+
+# Load data
+genomic_data = load_model.GenomicDataVDJ()
+genomic_data.load_igor_genomic_data(params_file_name, V_anchor_pos_file, J_anchor_pos_file)
+ #Load model
+generative_model = load_model.GenerativeModelVDJ()
+generative_model.load_and_process_igor_model(marginals_file_name)
+
+#Process model/data for pgen computation by instantiating GenerationProbabilityVDJ
+pgen_model = pgen.GenerationProbabilityVDJ(generative_model, genomic_data)
+
+for clone_id in args.clone_ids.split(','):
+    base_string = args.directory + "/" + args.id + "_" + clone_id
+    if args.quiet > 0:
+        print("\nRunning on clone", clone_id, "with saved data in", base_string)
+
+    # get the input data from the saved location 
+    clone_number = clone_id
+    df_string= "/sample/sample_lineages_sample_pars_hlp_rootprobs.txt"
+    table_path = base_string + df_string
+    igphyml_df = pd.read_csv(table_path, sep = "\t", header = None)
+    # the other columns here will be useful later but not now
+    igphyml_df.columns = ["site", "codon", "partial_likelihood", "nope", "nada", "no", "equilibrium"]
+    igphyml_df['value'] = igphyml_df['partial_likelihood'] * igphyml_df['equilibrium']
+
+    germline_string = base_string + "/olga_testing_germline.txt"
+    cdr3_string = base_string + "/olga_testing_germline_cdr3.txt"
+
+    #junction_length_string = base_string + "/olga_testing_germline_junc_len.txt"
+    with open(germline_string, "r") as f:
+        starting_germline = f.read().strip()
+
+    with open(cdr3_string, "r") as f:
+        starting_cdr3 = f.read().strip()
+
+    #with open(junction_length_string, "r") as f:
+    #    junction_length = f.read().strip()
+    junction_length = len(starting_cdr3)
+
+    # check for Ns in the V and J gene 
+    v_gene = starting_germline.split(starting_cdr3)[0]
+    j_gene = starting_germline.split(starting_cdr3)[1]
+    if "N" in v_gene or "N" in j_gene:
+        if(args.quiet > 0):
+            print("N in V or J gene -- adding to igphyml df")
+        codons = [starting_germline[i:i+3] for i in range(0, len(starting_germline), 3)]
+        indices_with_N = [index for index, value in enumerate(codons) if 'N' in value and not (len(v_gene) // 3 + 1 <= index <= len(codons) - len(j_gene) // 3  - 1)]
+        new_rows = []
+        for index in indices_with_N:
+            # get the sum of the values at that site 
+            sub = igphyml_df[igphyml_df['site'] == index]
+            sub_sum = sub['value'].sum()
+            new_rows.append({'site': index, 'codon': codons[index], 'partial_likelihood': sub_sum, 'nope': 0, 'nada': 0, 'no': 0, 'equilibrium': 0, 'value': 0})
+        igphyml_df = pd.concat([igphyml_df, pd.DataFrame(new_rows)], ignore_index=True)
+        igphyml_df = igphyml_df.sort_values(by='site', ascending=True)
+        # replace the Ns in the cdr3 with C and stitch it back together
+        cdr3 = starting_germline.split(v_gene)
+        cdr3  = cdr3[1].split(j_gene)[0]
+        starting_germline = v_gene + cdr3.replace("N", "C") + j_gene
+
+    if "N" in starting_cdr3:
+        if(args.quiet > 0):
+            print("N in starting CDR3 -- replacing with C")
+        starting_cdr3 = starting_cdr3.replace("N", "C")
+        # replace the Ns in the cdr3 with C and stitch it back together
+        starting_germline = v_gene + starting_cdr3 + j_gene
+
+    # split the germline into a list of codons
+    codons = [starting_germline[i:i+3] for i in range(0, len(starting_germline), 3)]
+
+    # TODO: if the model (pgen) files aren't included and need to be read in, the argv calls here will need to be updated
+    values = get_updated_germline(starting_germline, starting_cdr3, igphyml_df, pgen_model, junction_length, cdr3_only=True, max_iter=int(args.max_iters), nproc = int(args.nproc))
+
+    new_germline = values[0]
+    new_lhoods = values[1]
+    data = values[2]
+
+    # save the above data
+    with open(base_string + "/UCA.txt", "w") as f:
+        f.write(new_germline + "\n")
+
+    with open(base_string + "/UCA_lhoods.txt", "w") as f:
+        f.write(str(new_lhoods) + "\n")
+
+    data.to_csv(base_string + "/UCA_data.csv", index=False)
